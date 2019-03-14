99c4b465a5c800e1fa06fba01428d5ced41aea27
h151699 <andrewj.day@hotmail.com>
2019-03-14 22:25:33 +0100

    add markComplete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todo from './components/Todo';
+import Header from './components/layout/Header';
 
 import './App.css';
 
@@ -11,13 +12,13 @@ class App extends Component {
       {
         id: 1, 
         title: 'Throw the trash', 
-        completed: true
+        completed: false
       }, 
 
       {
         id: 2, 
         title: 'Feed Bjørn', 
-        completed: true
+        completed: false
       },
 
       {
@@ -28,13 +29,31 @@ class App extends Component {
     ]
   }
 
+  // toggle complete
+  markComplete = (id) => {
+    this.setState({todos:this.state.todos.map(todo =>{
+      if(todo.id === id) {
+        todo.completed =!todo.completed
+
+      } 
+      return todo;
+    }) });
+  }
 
 
+
+
+//Delete Todo， filter out and return the not-deleted items
+delTodo = (id) => {
+  this.setState({todos: [...this.state.todos.filter(td =>  td.id !== id)]})
+}
   render() {
     return (
       <div className="App">
-  
-        <Todo todos={this.state.todos}/>
+
+        <Header />
+        <Todo todos={this.state.todos} markComplete = {this.markComplete}
+          delTodo = {this.delTodo}/>
 
 
       </div>
