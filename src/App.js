90b689ff9389af951e9cf2f9d54ce33f9aa5fce1
h151699 <andrewj.day@hotmail.com>
2019-03-14 23:32:45 +0100

    add new item(s)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Todo from './components/Todo';
 import Header from './components/layout/Header';
+import AddToDo from './components/AddToDo';
+import uuid from 'uuid';
 
 import './App.css';
 
@@ -10,19 +12,19 @@ class App extends Component {
     // objects
     todos: [  
       {
-        id: 1, 
+        id: uuid.v4(), 
         title: 'Throw the trash', 
         completed: false
       }, 
 
       {
-        id: 2, 
+        id:uuid.v4(), 
         title: 'Feed Bjørn', 
         completed: false
       },
 
       {
-        id: 3, 
+        id: uuid.v4(), 
         title: 'Dinner with Siri', 
         completed: false
       }
@@ -47,11 +49,24 @@ class App extends Component {
 delTodo = (id) => {
   this.setState({todos: [...this.state.todos.filter(td =>  td.id !== id)]})
 }
+
+// Add Todo
+addTodo = (title) => {
+  const newTodo ={
+    id: uuid.v4(), // install uuid to get the random id
+    
+    title, 
+    completed: false
+  }
+  this.setState({todos: [...this.state.todos, newTodo] });
+}
+
   render() {
     return (
       <div className="App">
-
+        
         <Header />
+        <AddToDo  addTodo = {this.addTodo}/>
         <Todo todos={this.state.todos} markComplete = {this.markComplete}
           delTodo = {this.delTodo}/>
 
--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react'
 
 export class AddToDo extends Component {
+    // add state value
+    state = {
+        title: ''
+    }
+
+    // onChange state
+    onChange = (e) => this.setState({[e.target.name]: e.target.value});
+
+    // onSubmit
+    onSubmit = (e) => {
+        e.preventDefault(); // 
+        this.props.addTodo(this.state.title); // pass the data/title
+        this.setState({title:''});// clear field
+    }
+
   render() {
     return (
-      <form style ={{display: 'flex'}}>
+      <form 
+        onSubmit={this.onSubmit}
+        style ={{display: 'flex'}}>
           <input 
             type="text" 
             name="title" 
             style={{flex: '10', padding: '5px'}}
-            placeholder="Add ToDo item..." />
+            placeholder="Add ToDo item..." 
+            value ={this.state.title}
+            onChange={this.onChange}
+            />
+            
             
             <input 
                 type= "submit"
                 value="Submit"
                 className="btn"
                 style={{flex: 1}}
+
                 />
       </form>
     )
